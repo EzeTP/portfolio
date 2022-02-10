072d5d80c329881f45becd10d2e47020a2517151
EzeTP <tejada.eap@gmail.com>
2022-02-10 09:10:26 -0300

    create a data index

---
+++ src/data.js
@@ -0,0 +1,58 @@
+import imgn from "./assets/weatherapp.png";
+import rick from "./assets/rickandmortyapp.png";
+import js from "./assets/js.png";
+import react from "./assets/react.png";
+import bootstrap from "./assets/bootstrap.png";
+import nodejs from "./assets/nodejs.png";
+import redux from "./assets/redux.png";
+import sass from "./assets/sass.png";
+import css3 from "./assets/css3.png";
+import html from "./assets/html.png";
+import postgresql from "./assets/postgresql.png";
+
+export const featuredPortfolio = [
+  {
+    id: 1,
+    title: "Weather app",
+    img: imgn,
+    link: "https://www.google.com/",
+  },
+  {
+    id: 2,
+    title: "RyM app",
+    img: rick,
+    link: "https://www.google.com/",
+  },
+];
+
+export const webPortfolio = [
+  {
+    id: 1,
+    title: "Rick and Morty",
+    img: rick,
+    link: "https://www.google.com/",
+  },
+  {
+    id: 1,
+    title: "Weather app",
+    img: imgn,
+    link: "https://www.google.com/",
+  },
+  {
+    id: 3,
+    title: "Web E-commerce Design",
+    img: "https://cdn.dribbble.com/users/3637068/screenshots/15467010/media/69a3279182b75365013fe285733d1122.png?compress=1&resize=1200x900",
+  },
+];
+
+export const icons = [
+  { id: 1, title: "HTML5", img: html },
+  { id: 2, title: "CSS3", img: css3 },
+  { id: 3, title: "Bootstrap5", img: bootstrap },
+  { id: 4, title: "Sass", img: sass },
+  { id: 5, title: "Javascript", img: js },
+  { id: 6, title: "React", img: react },
+  { id: 7, title: "Redux", img: redux },
+  { id: 8, title: "Node", img: nodejs },
+  { id: 9, title: "Postgresql", img: postgresql },
+];
